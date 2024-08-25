a1620b011016daed203db992c978eddd85244c54
zindsia <32201229+zindsia@users.noreply.github.com>
2024-08-25 21:21:41 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["zindsia.x3322.net"];
-pub const PUBLIC_RS_PUB_KEY: &str = "2kstH2tdQrbP9GMhMB7fftzv1zonKnmgAtJfgn4pl0g=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
